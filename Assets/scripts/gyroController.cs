09c91ebdf35eaee18fb868eae741eb6e83be7f57
tbstdt <tbstdt@gmail.com>
2019-08-06 17:13:53 +0500

    add accelerometer support

--- Assets/scripts/gyroController.cs
+++ Assets/scripts/gyroController.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class gyroController : MonoBehaviour {
 
@@ -12,6 +13,7 @@ public class gyroController : MonoBehaviour {
 	public float MaxAnglie = 10f;
 	[Range(0,1)]
 	public float Shift = 1f;
+	
 	public GameObject Rotator;
 	
 	void Start ()
@@ -33,21 +35,42 @@ public class gyroController : MonoBehaviour {
 
 	private void Update()
 	{
-		if (isGyroEnabled)
+		var rotate = Vector3.zero;
+		if (!isGyroEnabled)
 		{
-			var rotate = Input.gyro.rotationRateUnbiased;
-			rotate.z = 0;
-			RotateAndShift(rotate);
-
-			var angle = Quaternion.Angle( startRotation, Rotator.transform.rotation );
-			if (angle > MaxAnglie)
-				RotateAndShift(-rotate);
+			rotate = new Vector3(Input.gyro.rotationRateUnbiased.y,Input.gyro.rotationRateUnbiased.x, 0);
 		}
+		else
+		{
+			rotate = new Vector3(Input.acceleration.x, -Input.acceleration.y, 0);
+		}
+		
+		RotateAndShift(rotate);
+		CheckBounds(rotate);
 	}
-
+	
+	
 	private void RotateAndShift(Vector3 rotate)
 	{
 		Rotator.transform.Rotate(rotate * Accuracy);
-		Rotator.transform.position += new Vector3(rotate.y, rotate.x) * Shift;
+		Rotator.transform.position += rotate * Shift;
+	}
+
+	private void CheckBounds(Vector3 rotate)
+	{
+		var angle = Quaternion.Angle(startRotation, Rotator.transform.rotation);
+		if (angle > MaxAnglie)
+			RotateAndShift(-rotate);
+	}
+
+	
+	
+	private void OnGUI()
+	{
+		var style = new GUIStyle();
+		style.fontSize = 36;
+		style.normal.textColor = Color.white;
+		GUI.Label(new Rect(10, 10, 200, 50),  Input.acceleration.ToString(), style);
+		GUI.Label(new Rect(10, 70, 200, 50),  Input.gyro.rotationRateUnbiased.ToString(), style);
 	}
 }
